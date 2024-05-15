c5ba7735825e296b986baccc285d809a790e2dd4
niyayesh monsef <nmonsef@myseneca.ca>
2024-05-15 09:02:36 -0400

    added lab1d.

---
+++ lab1d.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+x = 10
+y = 2
+z = 5
+result = sum([x + y * z])
+
+# f-strings are used to format strings with expressions
+#Print out the result along with variable names and their values
+print(f"{x} + {y} * {z} = {result}")
